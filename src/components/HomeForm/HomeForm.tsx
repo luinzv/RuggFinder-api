6193a0b21116c730a16d6237271042c2832268ec
Luis <luis.contreras.dev.02@gmail.com>
2025-12-25 16:58:12 -0300

    Cambios en el home

--- src/components/HomeForm/HomeForm.tsx
+++ src/components/HomeForm/HomeForm.tsx
@@ -1,25 +1,80 @@
-import styles from './HomeForm.module.css';
-import peticon from '../../assets/petqr-icon.png';
-import { Link } from 'react-router-dom';
+import styles from "./HomeForm.module.css";
+import { backgroundCathuman, petqrIcon, stampImage, userSvg } from "../../assets/icon";
 
 export const HomeForm = () => {
+
   return (
-    <div className={styles.header}>
-      <div className={styles.logoSection}>
-        <div className={styles.logoContainer}>
-          <img src={peticon} alt="pet" className={styles.petqricon} />
+    <div className={styles.Header}>
+      <div className={styles.navbarContainer}>
+        <div className={styles.navbarMenu}>
+          <div className={styles.stamp}>
+            <div className={styles.stampImage}>
+              <img src={stampImage} alt="fingerprint" />
+            </div>
+            <div className={styles.stampText}>
+              RuggFinder
+            </div>
+          </div>
+
+          <div className={styles.aboutContainerNav}>
+            <a href="#">Inicio</a>
+            <a href="#">Nosotros</a>
+            <a href="#">Contacto</a>
+          </div>
+
+
+          <div className={styles.login}>
+            <div className={styles.containerUserSvg}>
+              <img src={userSvg} alt="userSvg" />
+            </div>
+            <h3>Ingresar</h3>
+          </div>
         </div>
-        <h1 className={styles.title}>Crear una cuenta</h1>
-        <h3 className={styles.description}>Únete para comenzar</h3>
+      </div>
+      <div className={styles.ContainerGridForm}>
+        <div className={styles.logoSection}>
+          <div className={styles.logoContainer}>
+            <img src={petqrIcon} alt="petQRicon" />
+          </div>
+        <h1 className={styles.title}>
+          Crear una cuenta
+        </h1>
+        <h3 className={styles.description}>Unete para comenzar</h3>
+        </div>
+
+        <div className={styles.FormContainer}>
+          <div className={styles.formRegister}>
+            <a href="/register" className={styles.signEmail}>Registrarse con correo</a>
+            <a href="/login" className={styles.login}>Iniciar sesión</a>
+            <a href="#" className={styles.privacyText}>Politicas de privacidad</a>
+          </div>
+        </div>
       </div>
 
-      <div className={styles.loginSection}>
-        <div className={styles.formWrapper}>
-          <Link to="/register" className={styles.signEmail}>Registrarse con correo</Link>
-          <Link to="/login" className={styles.login}>Iniciar sesión</Link>
-          <a href="#" className={styles.policy}>Políticas de privacidad</a>
+      <div className={styles.aboutSectionContainer}>
+        <div className={styles.aboutSectionContainer}>
+          <div className={styles.visualWrapper}>
+
+            <div className={styles.imageWrapper}>
+              <img src={backgroundCathuman} alt="PetQR" />
+            </div>
+
+            <div className={styles.textWrapper}>
+              <h1>Accede a tu cuenta</h1>
+              <h3>Inicia sesión para revisar los perfiles</h3>
+            </div>
+          </div>
+          <div className={styles.buttonStyles}>
+            <button className={`${styles.btnblack} ${styles.buttons}`}>
+              Ingresar
+            </button>
+            <button className={`${styles.btnwhite} ${styles.buttons}`}>
+              Registrarse
+            </button>
+          </div>
         </div>
       </div>
+
     </div>
   );
 };
